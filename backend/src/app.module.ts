1cc11706a911ae9b87a2d64a708d70ce90629e98
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-15 10:15:08 +0200

    add: nest modules controllers and services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SkillsModule } from './skills/skills.module';
+import { ExperienceModule } from './experience/experience.module';
+import { ContactModule } from './contact/contact.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [],
+  imports: [SkillsModule, ExperienceModule, ContactModule, StatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
