384aff42b4ed3f4141a2a55e07dca2349c8b65f8
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-22 12:39:52 +0200

    favicon

---
+++ dist/portfolio2026_A/server/chunk-BRNIAFPA.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Cb as o,Db as a,_b as p,bb as n}from"./chunk-LU66BMRM.mjs";import"./chunk-T55IDOPT.mjs";var m=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-lab"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"lab works!"),a())},encapsulation:2})};export{m as LabComponent};
