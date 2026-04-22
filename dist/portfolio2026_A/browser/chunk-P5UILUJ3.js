4b54d8f9543a1b0c73e3a920569279304103fc48
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-22 14:27:15 +0200

    deploy

--- dist/portfolio2026_A/browser/chunk-P5UILUJ3.js
+++
@@ -1,1 +0,0 @@
-import{Bb as o,Cb as a,Zb as p,cb as n}from"./chunk-GNSMTJE6.js";var m=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-lab"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"lab works!"),a())},encapsulation:2})};export{m as LabComponent};
---
+++ dist/portfolio2026_A/server/chunk-DUY6UCF3.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Eb as o,Fb as a,ac as p,cb as n}from"./chunk-FFVEDGQV.mjs";import"./chunk-T55IDOPT.mjs";var m=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-lab"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"lab works!"),a())},encapsulation:2})};export{m as LabComponent};
