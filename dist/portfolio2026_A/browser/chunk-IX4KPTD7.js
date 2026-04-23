ead80ccf1a04ce87e8bc61744c69cd27cd057ff6
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-23 09:12:55 +0200

    deploy

---
+++ dist/portfolio2026_A/browser/chunk-IX4KPTD7.js
@@ -0,0 +1,1 @@
+import{Eb as o,Fb as a,bc as p,eb as n}from"./chunk-RM6YBK7I.js";var m=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-lab"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"lab works!"),a())},encapsulation:2})};export{m as LabComponent};
--- dist/portfolio2026_A/server/angular-app-manifest.mjs
+++ dist/portfolio2026_A/server/angular-app-manifest.mjs
@@ -8,45 +8,45 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-IHTE34KX.js",
-      "chunk-SZHH56J3.js",
-      "chunk-4EQNT36A.js"
+      "chunk-OPRF5VKP.js",
+      "chunk-DGTNUMAM.js",
+      "chunk-I3PXBMHQ.js"
     ],
     "route": "/"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-G2ZCCVXT.js",
-      "chunk-4EQNT36A.js"
+      "chunk-KBNBR2NL.js",
+      "chunk-I3PXBMHQ.js"
     ],
     "route": "/contact"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-3M7VJGHE.js"
+      "chunk-Z3RVBXTQ.js"
     ],
     "route": "/about-me"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-3XVIISCK.js"
+      "chunk-ACFRWWBY.js"
     ],
     "route": "/error"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-ZITVFO7U.js"
+      "chunk-IX4KPTD7.js"
     ],
     "route": "/lab"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-EN3NDFZN.js"
+      "chunk-WQV7MGIW.js"
     ],
     "route": "/legal"
   },
@@ -58,14 +58,14 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 67190, hash: '025348e8ae81871ecbf697cf6edef89fa775f0c296ba79fc7e31b80b14dea327', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 13327, hash: '3388c7f7568f4118868452ccf1ed5d8525a5548f42325d6e3b556bbd244e1628', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'legal/index.html': {size: 120729, hash: '487a307423d0d1eecc1bcdd9c555e9455f16f46471285fd62f832daf0a354d81', text: () => import('./assets-chunks/legal_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 167626, hash: '7df14c1bc5f7748af278ebed892cf2c69bec65840d63fb4c5074a096aebc115b', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'about-me/index.html': {size: 122879, hash: 'bb202dd9a2e29462252c26fb0ff5733ed4a70f4b98afc6198959a3332b6d2675', text: () => import('./assets-chunks/about-me_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 172518, hash: '56dd8228cc0270c09e7fd1390c60f328d41ad61ac5711479d5353bd6b0e581ca', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'lab/index.html': {size: 119420, hash: '86264a3060e86ff8158dcd796f7c321c503f6bfd12bb41348d2a67d1e1124a96', text: () => import('./assets-chunks/lab_index_html.mjs').then(m => m.default)},
-    'error/index.html': {size: 123227, hash: '532600471cc430f96e82295414b77c71334431c133dcd47b0666f4c080ae42a0', text: () => import('./assets-chunks/error_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 67241, hash: 'a1262b307c7551b93b3644f6d542be973c90c1a05d1d27ed61c1509ed41e3de1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 13378, hash: '049946e1dfb43166d6c6dc2e24f30257e480ba3da0f930bb26b7cb76d20a565c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'about-me/index.html': {size: 122895, hash: '3ae12236b0a837d99d86adc341f71225754dfe15d6eeca2abe877b298fc783ee', text: () => import('./assets-chunks/about-me_index_html.mjs').then(m => m.default)},
+    'legal/index.html': {size: 120745, hash: 'ad0d35c502a34c1177afb3b1d1112d0b6b96cfbb84498bf04fdd9c058cf95305', text: () => import('./assets-chunks/legal_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 167826, hash: '51108e3673d58c3b7e4a8d59a9cea260fb729be3b076c4b90c1434360b045e7f', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 172130, hash: 'a0d1932fefa311ebebef63a586b723c041dfa3d6a85b15165356e769fff808ee', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'error/index.html': {size: 123237, hash: 'd5d2504ab26fe995c544f7da969915df67e3706c76382fba28991c7038579e9a', text: () => import('./assets-chunks/error_index_html.mjs').then(m => m.default)},
+    'lab/index.html': {size: 119436, hash: 'cd209805ba28ae3a5ce87b8ecb3319d3188ba85cf0145ccd1a86131e96e283c8', text: () => import('./assets-chunks/lab_index_html.mjs').then(m => m.default)},
     'styles-TBBLIE2T.css': {size: 91501, hash: 't+6B9jiWQ1M', text: () => import('./assets-chunks/styles-TBBLIE2T_css.mjs').then(m => m.default)}
   },
 };
