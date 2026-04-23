95e18b480d0f869512dc773a29e7340db0fc5b96
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-23 09:43:46 +0200

    deploy-api

--- backend/dist/status/data/status.data.js
+++ backend/dist/status/data/status.data.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.STATUS = void 0;
 const status_contants_1 = require("../constants/status.contants");
-exports.STATUS = status_contants_1.OFFLINE;
+exports.STATUS = status_contants_1.ONLINE;
 //# sourceMappingURL=status.data.js.map
