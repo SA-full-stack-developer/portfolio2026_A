933e8751d05ca511ab33ff71580d7c2d827c67a9
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-23 09:11:35 +0200

    deploy-api

--- backend/dist/status/status.controller.d.ts
+++ backend/dist/status/status.controller.d.ts
@@ -2,8 +2,5 @@ import { StatusService } from './status.service';
 export declare class StatusController {
     private readonly statusService;
     constructor(statusService: StatusService);
-    getStatus(): {
-        status: import("./entities/status.entity").Status;
-        uptime: number;
-    };
+    getStatus(): import("./entities/status.entity").Status;
 }
