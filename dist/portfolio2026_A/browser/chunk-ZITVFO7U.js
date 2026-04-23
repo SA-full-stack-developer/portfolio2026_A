5c98fc9a7e14a6a7255c37cc8bde660a3e67e709
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-23 09:12:55 +0200

    deploy

--- dist/portfolio2026_A/browser/chunk-ZITVFO7U.js
+++
@@ -1,1 +0,0 @@
-import{$b as p,Db as o,Eb as a,db as n}from"./chunk-JGUAYAQD.js";var m=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-lab"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"lab works!"),a())},encapsulation:2})};export{m as LabComponent};
