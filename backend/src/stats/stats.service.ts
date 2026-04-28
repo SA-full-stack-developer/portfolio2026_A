6c974af5ee67bbdfd325503bb991f850c2a32521
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-04-28 18:33:09 +0200

    cleaning

--- backend/src/stats/stats.service.ts
+++ backend/src/stats/stats.service.ts
@@ -75,7 +75,7 @@ export class StatsService {
         return diff * multiplier;
 
       case StatKind.SERVICE:
-        return this.skillsService.getHighlightedCount() * multiplier + 4444;
+        return this.skillsService.getHighlightedCount() * multiplier;
 
       default:
         return 0;
