7e102552b7f986c1d48e36f4edf12f9aff46f699
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-05-07 12:40:06 +0200

    fix: server prerrender routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'error',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Server,
-  },
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'about-me', renderMode: RenderMode.Prerender },
+  { path: 'contact', renderMode: RenderMode.Prerender },
+  { path: 'legal', renderMode: RenderMode.Prerender },
+  { path: 'lab', renderMode: RenderMode.Prerender },
+  { path: 'error', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Server },
 ];
