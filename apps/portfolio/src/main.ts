1699dc6dd323fdc8ae6a411bdef82c673654ff6a
SA-full-stack-developer <salcedorodriguezcristian@gmail.com>
2026-05-22 11:28:02 +0200

    fix: eslint

--- apps/portfolio/src/main.ts
+++ apps/portfolio/src/main.ts
@@ -1,10 +1,9 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-// Solo inicializa federation en el browser
 if (typeof window !== 'undefined') {
   initFederation()
     .catch((err) => console.error(err))
-    .then((_) => import('./bootstrap'))
+    .then(() => import('./bootstrap'))
     .catch((err) => console.error(err));
 } else {
   import('./bootstrap');
